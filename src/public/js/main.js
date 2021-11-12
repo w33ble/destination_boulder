2d2988803033427c7eb268ca7da26af68021b629
Joe Fleming <github@joefleming.net>
2021-11-11 19:43:10 -0700

    chore: prepare for heroku deploy

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,6 +1,10 @@
-import Alpine from 'alpinejs'
+import Alpine from 'https://unpkg.com/alpinejs@3.5.1/dist/module.esm.js'
 
 // attach to window, useful for debugging
 window.Alpine = Alpine
+
+document.addEventListener('alpine:init', () => {
+  console.log('alpine is started')
+})
 
 Alpine.start()
