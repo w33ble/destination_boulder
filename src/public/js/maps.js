4014cbe606e84f81a68f3557c49adc49f5351fb3
Joe Fleming <github@joefleming.net>
2021-11-15 19:36:27 -0700

    feat: load a map on the page

--- src/public/js/maps.js
+++ src/public/js/maps.js
@@ -1,9 +1,29 @@
 import gqlQuery from './gqlQuery.js'
+import { Loader } from 'https://cdn.jsdelivr.net/npm/@googlemaps/js-api-loader@1.12.9/dist/index.esm.js'
 
 document.addEventListener('alpine:init', async () => {
-  // Alpine.data('maps', () => ({
-  //   title: 'Maps'
-  // }))
+  Alpine.data('maps', () => ({
+    loaded: false,
+    async init() {
+      // initialize google maps
+      const options = {
+        center: {
+          lat: 40.0289103,
+          lng: -105.2377461,
+        },
+        zoom: 11,
+      }
+      const loader = new Loader({
+        apiKey: "AIzaSyDdxXbvuh1sCOR8rA6p1D6AQjI6EoGBUvM",
+        version: "weekly",
+        libraries: ["places"]
+      });
+      const google = await loader.load()
+
+      new google.maps.Map(document.getElementById("mapTarget"), options);
+      this.loaded = true
+    },
+  }))
 
   try {
     const client = gqlQuery('https://intense-sparrow-57.hasura.app/v1/graphql')
